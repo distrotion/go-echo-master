93c07b3b51d2e6f39e33631bf646a2d2256cb2ad
Thai Parkerizing <administrator@cpe-172-101-34-209.maine.res.rr.com>
2022-09-27 20:18:54 +0700

    .

--- flow/testflow/test.go
+++ flow/testflow/test.go
@@ -6,11 +6,18 @@ import (
 	"math/rand"
 	"net/http"
 	"reflect"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
 	"go.mongodb.org/mongo-driver/bson"
+)
+
+var (
+	memList01 = []string{}
+	memList02 = []string{}
 )
 
 func TESTPOST(c echo.Context) error {
@@ -37,12 +44,60 @@ func TESTPOSTBSON(c echo.Context) error {
 	return c.JSON(200, input)
 }
 
+func TESTcrash(c echo.Context) error {
+	fmt.Println(`--TESTPOST--`)
+	//------------------------------------------------------
+	input := toBSON(c)
+	//
+	inputUID := toDATABODY(input[`UID`])
+	output := `NOK`
+	//------------------------------------------------------------------------- airbag
+	if stringInSlice(inputUID.Str, memList01) == false {
+		memList01 = append(memList01, inputUID.Str)
+		output = `OK`
+		//----------------------------------------------------------------------- airbag
+
+		time.Sleep(20 * time.Second)
+		fmt.Println("Sleep Over.....")
+
+		//----------------------------------------------------------------------- airbag
+		list := remove(memList01, inputUID.Str)
+		memList01 = list
+	}
+
+	// list := remove(memList01, inputUID.Str)
+	// memList01 = list
+	//------------------------------------------------------------------------- airbag
+	//------------------------------------------------------
+	return c.JSON(200, output)
+}
+
+func TESTcrashCLEAR_SUPER(c echo.Context) error {
+	fmt.Println(`--TESTPOST--`)
+	//------------------------------------------------------
+
+	//
+	memList01 = []string{}
+	memList02 = []string{}
+
+	output := `OK`
+	//------------------------------------------------------------------------- airbag
+
+	// list := remove(memList01, inputUID.Str)
+	// memList01 = list
+	//------------------------------------------------------------------------- airbag
+	//------------------------------------------------------
+	return c.JSON(200, output)
+}
+
 func UseSubroute(group *echo.Group) {
 	group.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "TESTFLOW")
 	})
 	group.POST("/test", TESTPOST)
 	group.POST("/testBSON", TESTPOSTBSON)
+	group.POST("/TESTcrash", TESTcrash)
+	group.POST("/TESTcrashCLEAR_SUPER", TESTcrashCLEAR_SUPER)
 }
 
 //insClinicProB := maindbv2.UpdateArchive(ctx, authDB, authDB_clinic_profile_model, bson.M{"SUID": sUID}, ClinicProB)
@@ -54,6 +109,27 @@ func UseSubroute(group *echo.Group) {
 // t2 := time.Now().UnixNano()
 //String >> fmt.Sprintf("%v", listPatient[i][`SFirstName`])
 //int32 >>  int(listPatient[i][`NImgAvatarID`].(int32))
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func remove(items []string, item string) []string {
+	newitems := []string{}
+
+	for _, i := range items {
+		if i != item {
+			newitems = append(newitems, i)
+		}
+	}
+
+	return newitems
+}
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
@@ -170,4 +246,107 @@ func primitivetoBsonM(input interface{}, my string) []bson.M {
 		return output
 	}
 
+}
+
+func jsonToMap(jsonStr string) map[string]interface{} {
+	result := make(map[string]interface{})
+	json.Unmarshal([]byte(jsonStr), &result)
+	return result
+}
+
+func UNIXstrToDayOfWeek(input string) time.Duration {
+	i, err := strconv.ParseInt(input, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	tm := time.Unix(i, 0)
+	// fmt.Println(tm)
+	return GetStartDayOfWeek(tm)
+}
+
+func GetStartDayOfWeek(tm time.Time) time.Duration { //get monday 00:00:00
+	weekday := time.Duration(tm.Weekday())
+	// if weekday == 0 {
+	// 	weekday = 7
+	// }
+	// year, month, day := tm.Date()
+	// currentZeroDay := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+	return weekday
+}
+
+func UNIXstrToYear(input string) time.Duration {
+	i, err := strconv.ParseInt(input, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	tm := time.Unix(i, 0)
+	// fmt.Println(tm)
+	return time.Duration(tm.Year())
+}
+
+type DATABODY struct {
+	Str   string `json:"str"`
+	Inte  int64  `json:"inte"`
+	Bools bool   `json:"bools"`
+}
+
+func toDATABODY(c interface{}) DATABODY {
+	var msg DATABODY
+
+	itf := intoBsonM(c)
+
+	intVar, _ := strconv.ParseInt(fmt.Sprintf("%v", itf[`inte`]), 0, 64)
+	boolVar := strings.ToUpper(fmt.Sprintf("%v", itf[`bools`]))
+	msg.Str = fmt.Sprintf("%v", itf[`str`])
+	msg.Inte = intVar
+	fmt.Println(boolVar)
+	if boolVar == `TRUE` {
+		msg.Bools = true
+	} else {
+		msg.Bools = false
+	}
+
+	return msg
+}
+
+func intoBsonM(t interface{}) bson.M {
+	var output bson.M
+	switch reflect.TypeOf(t).Kind() {
+	case reflect.Map:
+		s := reflect.ValueOf(t)
+		bs, err := json.Marshal(s.Interface())
+		if err != nil {
+			panic(err)
+		}
+		var o bson.M
+		if err := json.Unmarshal(bs, &o); err != nil {
+			panic(err)
+		}
+		//------------------------------
+		output = o
+
+	}
+	return output
+}
+
+func toBSON(c echo.Context) bson.M {
+
+	input := make(bson.M)
+	err := json.NewDecoder(c.Request().Body).Decode(&input)
+	if err != nil {
+		log.Error("empty json body")
+	}
+
+	return input
+}
+
+func toINTERFACE(c echo.Context) map[string]interface{} {
+
+	input := make(map[string]interface{})
+	err := json.NewDecoder(c.Request().Body).Decode(&input)
+	if err != nil {
+		log.Error("empty json body")
+	}
+
+	return input
 }
